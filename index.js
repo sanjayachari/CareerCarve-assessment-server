f25140b04f3e9f05e0fd7a4dde00b271aa7d666f
sanjay <devsanjay7676@gmail.com>
2024-08-14 13:57:01 +0530

    cors

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(cors({
     origin:"https://careercarve-assessment-server.onrender.com",
     // origin:"http://localhost:5173",
     credentials:true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 
 app.use(cookieParser());
