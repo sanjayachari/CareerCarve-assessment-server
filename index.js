8b62cdabd5ec4a0c89ce09be7cfeeeb6e2e470f1
sanjay <devsanjay7676@gmail.com>
2024-08-14 13:59:05 +0530

    cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(cors({
     // origin:"http://localhost:5173",
-    origin:"https://careercarve-assessment-server.onrender.com",
+    origin:"https://careercarve-assessment-client.onrender.com",
     // origin:"http://localhost:5173",
     credentials:true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
